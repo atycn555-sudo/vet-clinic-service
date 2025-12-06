14014c48b84321e33f9cfa3e128c17bd8b5bc6d1
atycn555-sudo <atycn555@gmail.com>
2025-12-05 20:28:56 -0600

    Update LoginRequest.java

--- src/main/java/com/example/vet/dto/LoginRequest.java
+++ src/main/java/com/example/vet/dto/LoginRequest.java
@@ -4,7 +4,6 @@ public class LoginRequest {
     private String username;
     private String password;
 
-    // Getters y Setters
     public String getUsername() {
         return username;
     }
